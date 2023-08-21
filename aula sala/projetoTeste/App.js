c7eaba697dd52980b181ef3e0ed0692fc767a91f
Thiago Barcelo <thiago33fb@gmail.com>
2023-08-21 20:38:30 -0300

    codego teste sala de aula

---
+++ aula sala/projetoTeste/App.js
@@ -0,0 +1,44 @@
+// npx create-expo-app projetoTeste
+// npm install --global expo-cli
+// cd AwesomeProject
+// npx expo start
+
+
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.testo}>Vamoooo poha!</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="DON'T TOUCH ME"
+        onPress={() => Alert.alert('poha...')}
+      />  
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  testo: {
+    // backgroundColor: "#333",
+    fontSize: 60,
+    textAlign:"center",
+
+  },
+});
