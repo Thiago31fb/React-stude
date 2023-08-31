d6d800efdcbe8c4a84cc0a95edc9c0321534c70f
Thiago Barcelo <thiago33fb@gmail.com>
2023-08-31 09:59:27 -0300

    tutorial matheus 06-CSS modules

--- tutorial-matheus-react/src/components/Frase.js
+++ tutorial-matheus-react/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from "./Frase.module.css";
+
 function Frase() {
   return (
-    <div>
-      <p>Este e um componente com uma frase!</p>
+    <div className={styles.fraseContainer}>
+      <p className={styles.fraseContent}>Este e um componente com uma frase!</p>
     </div>
   );
 }
