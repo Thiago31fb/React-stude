a1361acdbb572d3d85fa8ce8c37f11bc60611661
Thiago Barcelo <thiago33fb@gmail.com>
2023-09-02 10:06:01 -0300

    atv 01 calculadora de idade

--- aula sala/projetoTeste/App.jsx
+++ aula sala/projetoTeste/App.jsx
@@ -1,38 +1,49 @@
+// cd "aula sala"
 // npx create-expo-app projetoTeste
 // npm install --global expo-cli
-// cd AwesomeProject
+// cd projetoTeste
 // npx expo start
 
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [dataNascimento, setDataNascimento] = useState("");
+  const [idade, setIdade] = useState("");
+
+  const calcularIdade = () => {
+    const dataNascimentoArray = dataNascimento.split("-");
+    const anoNascimento = parseInt(dataNascimentoArray[0]);
+    const mesNascimento = parseInt(dataNascimentoArray[1]);
+    const diaNascimento = parseInt(dataNascimentoArray[2]);
+
+    const dataAtual = new Date();
+    const anoAtual = dataAtual.getFullYear();
+    const mesAtual = dataAtual.getMonth() + 1;
+    const diaAtual = dataAtual.getDate();
+
+    let idadeCalculada = anoAtual - anoNascimento;
+
+    if (
+      mesAtual < mesNascimento ||
+      (mesAtual === mesNascimento && diaAtual < diaNascimento)
+    ) {
+      idadeCalculada--;
+    }
+
+    setIdade(idadeCalculada.toString());
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.testo}>Vamoooo poha!</Text>
-      <StatusBar style="auto" />
-      <Button title="DON'T TOUCH ME" onPress={() => Alert.alert("poha...")} />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Calculadora de Idade</Text>
+      <TextInput
+        placeholder="Data de Nascimento (AAAA-MM-DD)"
+        value={dataNascimento}
+        onChangeText={(text) => setDataNascimento(text)}
+      />
+      <Button title="Calcular Idade" onPress={calcularIdade} />
+      <Text>Sua idade é: {idade} anos</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#444",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  testo: {
-    color: "chartreuse",
-    fontSize: 60,
-    textAlign: "center",
-  },
-});
