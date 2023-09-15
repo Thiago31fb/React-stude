cbd9bd9c7a63f5710ac7127a1a27da80428e532c
Thiago Barcelo <thiago33fb@gmail.com>
2023-09-15 10:23:51 -0300

    tutorial matheus 11 Passando eventos por props

--- tutorial-matheus-react/src/components/Evento.js
+++ tutorial-matheus-react/src/components/Evento.js
@@ -1,11 +1,17 @@
+import Button from "./eventos/Button"
+
 const Evento = ({numero}) => {
   function meuEvento() {
     console.log(`opa, fui ativado!${numero}`);
   }
+  function segundoEvento() {
+    console.log(`opa, fui ativado, segundo Evento!`);
+  }
   return (
     <div>
       <p>Clique para disparar um evento: </p>
-      <button onClick={meuEvento}>Ativar!</button>
+      <Button event={meuEvento } text="Primeiro evento" />
+      <Button event={segundoEvento} text="Segundo evento" />
     </div>
   );
 };
