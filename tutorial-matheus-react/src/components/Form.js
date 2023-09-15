60efeaca5cf1e2dcc4fe653273f8f7a5867021ef
Thiago Barcelo <thiago33fb@gmail.com>
2023-09-15 09:54:47 -0300

    tutorial matheus 10 useState

--- tutorial-matheus-react/src/components/Form.js
+++ tutorial-matheus-react/src/components/Form.js
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 const Form = () => {
   function cadastrarUsuario(e) {
     e.preventDefault();
+    console.log(name);
+    console.log(password);
+
     console.log(`Cadastrou o usuario!`);
   }
+
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
 
   return (
     <div>
       <h1>Meu cadastro:</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="DIgite seu nome" />
+          <label htmlFor="name">Nome: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            // value={name}
+            placeholder="DIgite seu nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Senha: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="DIgite sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <input type="submit" value="CADASTRAR" />
