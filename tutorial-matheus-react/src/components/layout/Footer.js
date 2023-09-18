a218f7e1d2dba6147fbf297f62548a37373e0540
Thiago Barcelo <thiago33fb@gmail.com>
2023-09-18 12:32:37 -0300

    tutorial matheus 16 React Icons

--- tutorial-matheus-react/src/components/layout/Footer.js
+++ tutorial-matheus-react/src/components/layout/Footer.js
@@ -1,7 +1,23 @@
+import { FaFacebookSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-    return ( 
-        <footer>Rodape</footer>
-     );
-}
- 
+  return (
+    <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebookSquare />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+      </ul>
+      <p>nosaso Rodape</p>
+    </footer>
+  );
+};
+
 export default Footer;
