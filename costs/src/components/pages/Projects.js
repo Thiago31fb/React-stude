385c4f3c3a54185d9cf78308cb80b982d1237069
Thiago Barcelo <thiago33fb@gmail.com>
2023-09-29 11:42:40 -0300

    tutorial matheus 26 Criando Dashboard

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,6 +1,11 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import styles from "./Projects.module.css";
+
 
 const Projects = () => {
   const location = useLocation();
@@ -10,9 +15,15 @@ const Projects = () => {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
-      {message && <Message type="success" msg={message} /> } 
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 };
