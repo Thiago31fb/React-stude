881ea7f465363f4820305dd51ed39d7f7c4c0e72
Thiago Barcelo <thiago33fb@gmail.com>
2023-12-18 10:36:06 -0300

    tutorial react net-ninja 07  Click Events

--- tutorial-react-net-ninja/src/Home.js
+++ tutorial-react-net-ninja/src/Home.js
@@ -1,8 +1,26 @@
 const Home = () => {
-    return ( 
-    <div className="home">
-       <h1>app content</h1> 
-    </div> 
+
+    const handleClick01 = (name, e) =>{
+        console.log("Hello ninjas " + name);
+        console.log(e.target);
+    }
+
+     const handleClick = (e) => {
+       console.log("hello ninjas", e);
+     };
+
+     const handleClickAgain = (name, e) => {
+       console.log("hello " + name, e.target);
+     };
+    return (
+      <div className="home">
+        <h1>app content</h1>
+        <button onClick={(e) => handleClick01("Yoshi", e)}>click me</button>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickAgain("mario", e)}>
+          Click me again
+        </button>
+      </div>
     );
 }
  
