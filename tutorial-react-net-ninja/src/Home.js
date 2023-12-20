fa5c436ccda61e0a7227ea84780065e859f28c41
Thiago Barcelo <thiago33fb@gmail.com>
2023-12-20 09:20:20 -0300

    tutorial react net-ninja 11 Props

--- tutorial-react-net-ninja/src/Home.js
+++ tutorial-react-net-ninja/src/Home.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
@@ -13,12 +15,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 };
