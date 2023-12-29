499e6cdda1792ab3ac4fb979f68589da6ebc6073
Thiago Barcelo <thiago33fb@gmail.com>
2023-12-29 10:43:00 -0300

    tutorial react net-ninja 19 - Handling Fetch Errors

--- tutorial-react-net-ninja/src/Home.js
+++ tutorial-react-net-ninja/src/Home.js
@@ -3,8 +3,10 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  
   const [isPending, setIsPending] = useState(true);
+
+  const [erro, setErro] = useState(null);
 
   const handleDelete = (id) => {
     const newBLogs = blogs.filter((blog) => blog.id !== id);
@@ -14,18 +16,31 @@ const Home = () => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+    
+          if(!res.ok){
+            throw Error("Nao foi possivel buscar os dados para esse recurso...")
+          }
           return res.json();
         })
         .then((data) => {
           // console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setErro(null)
+
+        })
+        .catch((erro) => {
+          console.log(erro.message);
+          setErro(erro.message);
+          setIsPending(false)
         });
     }, 1200);
   }, []);
 
   return (
     <div className="home">
+
+      {erro && <div>{erro}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList
