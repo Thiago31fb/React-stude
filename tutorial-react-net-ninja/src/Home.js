d2eae54bd0c8381ce7090c0883463158b753d829
Thiago Barcelo <thiago33fb@gmail.com>
2024-01-02 09:44:07 -0300

    tutorial react net-ninja 20 -  Making a Custom Hook

--- tutorial-react-net-ninja/src/Home.js
+++ tutorial-react-net-ninja/src/Home.js
@@ -1,52 +1,21 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
+ const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+
+
   
-  const [isPending, setIsPending] = useState(true);
-
-  const [erro, setErro] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBLogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBLogs);
-  };
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-    
-          if(!res.ok){
-            throw Error("Nao foi possivel buscar os dados para esse recurso...")
-          }
-          return res.json();
-        })
-        .then((data) => {
-          // console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-          setErro(null)
-
-        })
-        .catch((erro) => {
-          console.log(erro.message);
-          setErro(erro.message);
-          setIsPending(false)
-        });
-    }, 1200);
-  }, []);
-
   return (
     <div className="home">
 
-      {erro && <div>{erro}</div>}
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList
           blogs={blogs}
           title="All Blogs!"
-          handleDelete={handleDelete}
         />
       )}
     </div>
---
+++ tutorial-react-net-ninja/src/useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [erro, setErro] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(
+              "Nao foi possivel buscar os dados para esse recurso..."
+            );
+          }
+          return res.json();
+        })
+        .then((data) => {
+          // console.log(data);
+          setData(data);
+          setIsPending(false);
+          setErro(null);
+        })
+        .catch((erro) => {
+          console.log(erro.message);
+          setErro(erro.message);
+          setIsPending(false);
+        });
+    }, 1200);
+  }, [url]);
+  return {
+    data,
+    isPending,
+    erro,
+  };
+};
+
+export default useFetch;
